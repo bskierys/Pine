e9decca53405d5401f75d6e98683a5901af496e5
bartlomiej.kierys <bskierys@gmail.com>
2016-11-09 20:54:08 +0100

    Creating library

---
+++ pine-sample/src/main/java/com/github/bskierys/pine/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.bskierys.pine.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.bskierys.pine.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
